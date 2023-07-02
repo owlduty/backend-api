b077647a3cdb52cc0ed29ff9b0c1bbb6b6d8e0f5
theredfish <did.julian@gmail.com>
2023-07-02 20:19:37 +0200

    Move auth payload to a function

--- db/migrations/2023-06-28-083128_auth_api/down.sql
+++ db/migrations/2023-06-28-083128_auth_api/down.sql
@@ -1,1 +1,3 @@
 drop function v1_0.signup;
+drop function v1_0.signin;
+drop function auth.payload;
--- db/migrations/2023-06-28-083128_auth_api/up.sql
+++ db/migrations/2023-06-28-083128_auth_api/up.sql
@@ -1,19 +1,10 @@
 set search_path to v1_0, public;
 
-create or replace function v1_0.signup(name text, email text, password text) returns json as $$
-declare
-    usr record;
+create or replace function auth.payload(usr auth.users) returns json as $$
 begin
-    insert into auth.users as u
-    (name, email, pass) values ($1, $2, $3)
-    returning *
-   	into usr;
-
     return json_build_object(
         'profile', json_build_object(
-            'id', usr.id,
-            'name', usr.name,
-            'email', usr.email
+            'id', usr.id
         ),
         'token', pgjwt.sign(
             json_build_object(
@@ -26,8 +17,39 @@ begin
     );
 end
 $$ security definer language plpgsql;
+revoke all privileges on function auth.payload(auth.users) from public;
+
+create or replace function v1_0.signup(name text, email text, password text) returns json as $$
+declare
+    usr auth.users;
+begin
+    insert into auth.users as u (name, email, pass) values ($1, $2, $3)
+    returning * into usr;
+
+    return auth.payload(usr);
+end
+$$ security definer language plpgsql;
 
 revoke all privileges on function v1_0.signup(text, text, text) from public;
 grant execute on function v1_0.signup(text,text,text) to anonymous;
+
+
+create or replace function v1_0.signin(email text, password text) returns json as $$
+declare
+    usr auth.users;
+begin
+	select * from auth.users as u
+    where u.email = $1 and u.pass = public.crypt($2, u.pass)
+   	INTO usr;
+
+    if usr is NULL then
+        raise exception 'invalid credentials';
+    else
+        return auth.payload(usr);
+    end if;
+end
+$$ stable security definer language plpgsql;
+revoke all privileges on function v1_0.signin(text, text) from public;
+grant execute on function v1_0.signin(text,text) to anonymous, webuser;
 
 set search_path to public;
