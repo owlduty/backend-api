378c1838e4cce11c07b67fe93bb22edcce9dc58a
theredfish <did.julian@gmail.com>
2023-07-06 13:28:48 +0200

    Add signup test and verify both user's id and JWT

--- tests/api/mod.rs
+++ tests/api/mod.rs
@@ -1,12 +1,13 @@
-use std::env;
-use std::panic::{catch_unwind, AssertUnwindSafe};
-
+use async_trait::async_trait;
 use dotenv::dotenv;
 use grillon::Grillon;
 use once_cell::sync::Lazy;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::env;
+use std::panic::{catch_unwind, AssertUnwindSafe};
 
 pub static GRILLON: Lazy<Grillon> = Lazy::new(|| {
-    // load .env variables
     dotenv().ok();
     let api_base_url = env::var("API_BASE_URL").expect("`API_BASE_URL` is missing from .env file");
 
@@ -20,25 +21,36 @@ pub static TOKIO_RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
         .expect("Failed to initialize tokio runtime")
 });
 
+pub static PG_POOL: Lazy<PgPool> = Lazy::new(|| {
+    dotenv().ok();
+    let db_uri = env::var("DATABASE_URL").expect("`DATABASE_URL` is missing from .env file");
+
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect_lazy(&db_uri)
+        .expect("Failed to initialize PostgreSQL connection pool")
+});
+
+#[async_trait]
 pub(crate) trait TestContext {
-    fn setup();
-    fn teardown();
-    fn run_test<F>(test: F) -> ()
+    async fn setup(&self);
+    async fn teardown(&self);
+    fn run_test<F>(&self, test: F) -> ()
     where
         F: std::future::Future,
     {
-        Self::setup();
+        TOKIO_RUNTIME.block_on(async { self.setup().await });
 
         let assert = AssertUnwindSafe(async { test.await });
         let result = catch_unwind(|| {
             TOKIO_RUNTIME.block_on(assert);
         });
 
-        Self::teardown();
+        TOKIO_RUNTIME.block_on(async { self.teardown().await });
 
         assert!(result.is_ok());
     }
 }
 
-mod auth;
+mod signup;
 mod synthetics;
---
+++ tests/api/signup.rs
@@ -0,0 +1,92 @@
+use super::TestContext;
+use crate::api::{GRILLON, PG_POOL};
+use async_trait::async_trait;
+use grillon::{dsl::is, header, json, Assert};
+use std::future::Future;
+use uuid::Uuid;
+
+pub(crate) struct SignupCtx<'a> {
+    user_email: &'a str,
+}
+
+impl<'a> SignupCtx<'a> {
+    fn new(user_email: &'a str) -> Self {
+        Self { user_email }
+    }
+}
+
+#[async_trait]
+impl TestContext for SignupCtx<'_> {
+    async fn setup(&self) {}
+
+    async fn teardown(&self) {
+        let query_res = sqlx::query("DELETE FROM auth.users WHERE email = $1")
+            .bind(self.user_email)
+            .execute(&*PG_POOL)
+            .await;
+
+        match query_res {
+            Ok(res) if res.rows_affected() != 1 => {
+                eprintln!(
+                    "[TEARDOWN] Expected 1 row to be removed but got: {}",
+                    res.rows_affected()
+                )
+            }
+            Ok(_) => (),
+            Err(err) => eprintln!("[TEARDOWN] Query failed: {}", err),
+        };
+    }
+}
+
+fn user_signup(email: &str, name: &str, password: &str) -> impl Future<Output = Assert> {
+    GRILLON
+        .post("rpc/signup")
+        .headers(vec![(
+            header::CONTENT_TYPE,
+            header::HeaderValue::from_static("application/json"),
+        )])
+        .payload(json!({
+            "name": name,
+            "email": email,
+            "password": password
+        }))
+        .assert()
+}
+
+#[test]
+fn signup_success() {
+    let email = "john.doe@owlduty.com";
+    let assertion = async {
+        user_signup(email, "john.doe", "testpass")
+            .await
+            .status(is(200))
+            .assert_fn(|assert| {
+                assert!(assert.json.is_some());
+
+                let json_body = assert.json.as_ref().unwrap();
+                // check user's id
+                assert!(
+                    json_body["profile"]["id"].is_string(),
+                    "User id should be a string"
+                );
+
+                let uuid = &json_body["profile"]["id"].as_str().unwrap();
+                assert!(
+                    Uuid::parse_str(uuid).is_ok(),
+                    "User id should be a valid UUID v4"
+                );
+
+                // Check JWT token
+                assert!(
+                    json_body["token"].is_string(),
+                    "JWT token should be a string"
+                );
+                assert!(
+                    json_body["token"].to_string().len() > 1,
+                    "JWT token should be longer than 1 character"
+                )
+            })
+    };
+
+    SignupCtx::new(email).run_test(assertion);
+}
